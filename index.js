48fc0d07c98202414f0aba1dfb11433a4cae6293
Geea <Geea@DESKTOP-CHRJE5F>
2025-07-09 18:22:45 +0300

    feat: added EventListener to detect keyboard activity for the drum kit;
    renamed mp3 files to match image names for consistency.

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 var drumButtonsList = document.querySelectorAll(".drum")
 
 for(let i = 0; i< drumButtonsList.length; i++){
-    drumButtonsList[i].addEventListener("click", handleClick);
+    drumButtonsList[i].addEventListener("click", handleButtonClick);
 }
 
-function handleClick(){
-    alert("i got clicked");
+function handleButtonClick(){
+    var buttonValue = this.innerHTML;
+    makeSound(buttonValue);
+}
+
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+})
+
+function makeSound(key){
+    switch(key){
+        case "w":
+            let tom1Audio = new Audio('./sounds/tom1.mp3');
+            tom1Audio.play();
+        case "a":
+            let tom2Audio = new Audio('./sounds/tom2.mp3');
+            tom2Audio.play();
+        case "s":
+            let tom3Audio = new Audio('./sounds/tom3.mp3');
+            tom3Audio.play();
+        case "d":
+            let tom4Audio = new Audio('./sounds/tom4.mp3');
+            tom4Audio.play();
+        break;
+        case "j":
+            let crashAudio = new Audio('./sounds/crash.mp3');
+            crashAudio.play();
+        case "k":
+            let kickAudio = new Audio('./sounds/kick.mp3');
+            kickAudio.play();
+        case "l":
+            let snareAudio = new Audio('./sounds/snare.mp3');
+            snareAudio.play();
+        default:
+            console.log(buttonValue);
+    }
 }
