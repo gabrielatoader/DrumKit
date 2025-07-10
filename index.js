d878084681b31dec1ec85e64b16724ad7ec2f725
Geea <Geea@DESKTOP-CHRJE5F>
2025-07-10 14:08:42 +0300

    feat: removed two tom drums, added some simple beat patterns.

--- index.js
+++ index.js
@@ -18,33 +18,25 @@ document.addEventListener("keydown", function(event){
 
 function makeSound(key){
     switch(key){
-        case "w":
-            let tom1Audio = new Audio('./sounds/tom1.mp3');
-            tom1Audio.play();
-            break;
-        case "a":
-            let tom2Audio = new Audio('./sounds/tom2.mp3');
-            tom2Audio.play();
-            break;
-        case "s":
-            let tom3Audio = new Audio('./sounds/tom3.mp3');
-            tom3Audio.play();
-            break;
-        case "d":
-            let tom4Audio = new Audio('./sounds/tom4.mp3');
-            tom4Audio.play();
-            break;        
-        case "j":
-            let crashAudio = new Audio('./sounds/crash.mp3');
-            crashAudio.play();
-            break;
-        case "k":
+         case "a":
             let kickAudio = new Audio('./sounds/kick.mp3');
             kickAudio.play();
             break;
-        case "l":
+        case "s":
             let snareAudio = new Audio('./sounds/snare.mp3');
             snareAudio.play();
+            break;
+        case "j":
+            let tom1Audio = new Audio('./sounds/tom1.mp3');
+            tom1Audio.play();
+            break;
+        case "k":
+            let tom2Audio = new Audio('./sounds/tom2.mp3');
+            tom2Audio.play();
+            break;
+        case "l":
+            let crashAudio = new Audio('./sounds/crash.mp3');
+            crashAudio.play();
             break;
         default:
             console.log(buttonValue);
