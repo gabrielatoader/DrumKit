9fffbadee97d636b01cf98bfbc70f324ddf77cef
Geea <Geea@DESKTOP-CHRJE5F>
2025-07-10 13:11:59 +0300

    feat: added button animations; fixed: repaired makeSound function, added breaks to clauses in switch statement;

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ for(let i = 0; i< drumButtonsList.length; i++){
 
 function handleButtonClick(){
     var buttonValue = this.innerHTML;
+
     makeSound(buttonValue);
+    buttonAnimation(buttonValue);
 }
 
 document.addEventListener("keydown", function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 function makeSound(key){
@@ -18,26 +21,39 @@ function makeSound(key){
         case "w":
             let tom1Audio = new Audio('./sounds/tom1.mp3');
             tom1Audio.play();
+            break;
         case "a":
             let tom2Audio = new Audio('./sounds/tom2.mp3');
             tom2Audio.play();
+            break;
         case "s":
             let tom3Audio = new Audio('./sounds/tom3.mp3');
             tom3Audio.play();
+            break;
         case "d":
             let tom4Audio = new Audio('./sounds/tom4.mp3');
             tom4Audio.play();
-        break;
+            break;        
         case "j":
             let crashAudio = new Audio('./sounds/crash.mp3');
             crashAudio.play();
+            break;
         case "k":
             let kickAudio = new Audio('./sounds/kick.mp3');
             kickAudio.play();
+            break;
         case "l":
             let snareAudio = new Audio('./sounds/snare.mp3');
             snareAudio.play();
+            break;
         default:
             console.log(buttonValue);
     }
+}
+
+function buttonAnimation(key){
+    var activeButton = document.querySelector("." +  key);
+
+    activeButton.classList.add("pressed");
+    setTimeout(function(){activeButton.classList.remove("pressed")}, 100);
 }
